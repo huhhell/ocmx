6db9e7e351cb15c4c2a2551d6c7bfd7c4d927ac9
huhhell <vika20020807@mail.ru>
2024-04-15 19:46:52 +0700

    add post request to cart

--- src/assets/components/Cart/Cart.tsx
+++ src/assets/components/Cart/Cart.tsx
@@ -1,13 +1,60 @@
 import styled from "styled-components";
 import {Product} from "../../../App.tsx";
 import {useCart} from "../../context/ProductsContext.tsx";
-import InputMask from 'react-input-mask';
+// @ts-ignore
+import InputMask from "react-input-mask";
+import {ChangeEventHandler, MouseEventHandler, useEffect, useState} from "react";
+import axios from "axios";
+import Popup from "../Popup/Popup.tsx";
 
 interface Props {
     products: Product[];
 }
 
+export interface Response {
+    success: 0 | 1;
+    error?: string;
+}
+
 export default function Cart({products}: Props) {
+    const [phone, setPhone] = useState('');
+    const [response, setResponse] = useState<Response>();
+    const [isPopupShown, setIsPopupShown] = useState(false);
+    const cart = useCart();
+
+
+    useEffect(() => {
+        let timeout: number;
+        if (isPopupShown) {
+            timeout = setTimeout(() => setIsPopupShown(false), 5000)
+        }
+
+        return () => clearTimeout(timeout)
+    })
+    const changePhoneHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setPhone(e.target.value);
+    }
+
+    const sendFormHandler: MouseEventHandler<HTMLButtonElement> = (e) => {
+        e.preventDefault();
+        const phoneWithoutSymbols = phone.replace(/[+()-]/g, '').replace(/ +/g, '');
+
+        const dataToSend = {
+            phone: phoneWithoutSymbols,
+            cart: cart
+        }
+
+        try {
+            axios.post('http://o-complex.com:1337/order', dataToSend)
+                .then(response => {
+                    setResponse(response.data);
+                    setIsPopupShown(true);
+                });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     const items = useCart();
 
 const itemsInCart = items.map(i => {
@@ -23,18 +70,21 @@ const itemsInCart = items.map(i => {
     </Item>
 })
 
-    return <Container>
-        <Title>Добавленные товары</Title>
-        {items.length === 0 ?
+    return <>
+        <Container>
+            <Title>Добавленные товары</Title>
+            {items.length === 0 ?
             <EmptyText>Корзина пуста</EmptyText> :
             <List>
                 {itemsInCart}
             </List>}
-        <Form action='#'>
-            <PhoneInput mask='+7 (999) 999-9999' placeholder='+7 (___) ___-____'/>
-            <SubmitButton>Заказать</SubmitButton>
-        </Form>
-    </Container>
+            <Form action='#'>
+                <PhoneInput mask='+7 (999) 999-9999' placeholder='+7 (___) ___-____' onChange={changePhoneHandler}/>
+                <SubmitButton onClick={sendFormHandler}>Заказать</SubmitButton>
+            </Form>
+        </Container>
+        { isPopupShown && <Popup response={response}/>}
+    </>
 }
 
 function getProductWithId(id: number, products: Product[]) {
@@ -67,6 +117,7 @@ const List = styled.ul`
     display: flex;
     flex-direction: column;
     gap: 10px;
+    margin-bottom: 30px;
 `
 
 const Item = styled.li`
